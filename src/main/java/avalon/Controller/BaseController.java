973db19478854529bac89fdc5f4aa0b58b7a1486
Bosco H <y95han@gmail.com>
2019-01-12 15:29:54 -0500

    Working sample controller

--- src/main/java/avalon/Controller/BaseController.java
+++ src/main/java/avalon/Controller/BaseController.java
@@ -1,14 +1,22 @@
 package avalon.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.atomic.AtomicLong;
 
+import avalon.Models.Greeting;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class BaseController {
 
-    @RequestMapping("/home")
-    public String test() {
-        return"welcome";
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello-world")
+    @ResponseBody
+    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
